6ef46e25d9a1c0d7385868db3315cee98e707246
Duncan Morrison <duncanmorrison2001@yahoo.com>
2020-12-07 11:42:50 +0000

    initial commit

---
+++ src/main/java/com/makers/pairingapp/PairingAppApplication.java
@@ -0,0 +1,13 @@
+package com.makers.pairingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairingAppApplication.class, args);
+	}
+
+}
