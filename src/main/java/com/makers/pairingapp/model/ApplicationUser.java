670305dc052704aaa9081481035f6245cc1951e9
Duncan Morrison <duncanmorrison2001@yahoo.com>
2020-12-09 12:37:59 +0000

    added jwt authentication

---
+++ src/main/java/com/makers/pairingapp/model/ApplicationUser.java
@@ -0,0 +1,41 @@
+package com.makers.pairingapp.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "applicationuser")
+public class ApplicationUser {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  private String username;
+  private String password;
+
+  public long getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String toString() {
+    return "ApplicationUser{" +
+            "id=" + id +
+            ", username='" + username + '\'' +
+            '}';
+  }
+}
